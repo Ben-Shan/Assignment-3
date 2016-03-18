0d39af489dbbc254589adcb575d006aa31f4c12e
desdagg <C14347601@mydit.ie>
2016-03-18 23:25:19 +0000

    sound play test

---
+++ visualiserSound/src/sound/SoundTest.java
@@ -0,0 +1,12 @@
+package sound;
+
+public class SoundTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		MakeSound mySound = new MakeSound();
+		mySound.playSound("MainTitle.wav");
+
+	}
+
+}
